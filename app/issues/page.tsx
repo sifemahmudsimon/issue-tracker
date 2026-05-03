28691563abe03950a79d04e4541e71bb246e4141
simon <simon@mirinfosys.com>
2026-05-04 02:16:51 +0600

    New Issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 function IssuesPage() {
-  return <Button>New Issue</Button>;
+  return (
+    <Button>
+      <Link href={"/issues/new"}>New Issues</Link>
+    </Button>
+  );
 }
 
 export default IssuesPage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Theme>
           <Navbar />
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 p-5">{children}</main>
         </Theme>
       </body>
     </html>
