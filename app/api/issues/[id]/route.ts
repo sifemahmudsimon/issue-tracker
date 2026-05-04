a1e57ffcc4fdc211af62f20420c733762e85a1af
simon <simon@mirinfosys.com>
2026-05-05 01:47:36 +0600

    build an api for updating issues

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,35 @@
+import { issueSchema } from "@/app/validationSchemas";
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await request.json();
+  const validation = issueSchema.safeParse(body);
+
+  if (!validation.success) {
+    const errors = validation.error.flatten().fieldErrors;
+    return NextResponse.json(errors, { status: 400 });
+  }
+
+  const { id } = await params;
+
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!issue)
+    return NextResponse.json({ error: "Invalid issue" }, { status: 404 });
+
+  const updatedIssue = await prisma.issue.update({
+    where: { id: issue.id },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updatedIssue, { status: 200 });
+}
