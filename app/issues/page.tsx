0307264500e70d1dee14d25216ef36b16409277d
simon <simon@mirinfosys.com>
2026-05-04 19:23:37 +0600

    issue added with mobile responsive

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,51 @@
-import { Button } from "@radix-ui/themes";
+import { prisma } from "@/lib/prisma";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
 
-function IssuesPage() {
+async function IssuesPage() {
+  const issues = await prisma.issue.findMany();
   return (
-    <Button>
-      <Link href={"/issues/new"}>New Issues</Link>
-    </Button>
+    <div>
+      <div className="mb-5">
+        <Button>
+          <Link href={"/issues/new"}>New Issues</Link>
+        </Button>
+      </div>
+
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Issue
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {issues.map((issue) => (
+            <Table.Row key={issue.id}>
+              <Table.Cell>
+                {issue.title}
+                <div className="block md:hidden p-2 bg-gray-200 rounded-md">
+                  {issue.status}
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {issue.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
   );
 }
 
