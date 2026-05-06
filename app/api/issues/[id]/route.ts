ec20bd53b055fe67119fae558c9449aee2cd9828
simon <simon@mirinfosys.com>
2026-05-07 01:33:58 +0600

    secure authorization

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,12 +1,18 @@
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/validationSchemas";
 import { prisma } from "@/lib/prisma";
 import delay from "delay";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
 
@@ -39,8 +45,12 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
 
-  await delay(2000)
+  await delay(2000);
   const { id } = await params;
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
