f6002be6c5ab1caff483b136f4e74b89cf617d3b
simon <simon@mirinfosys.com>
2026-05-11 11:23:26 +0600

    implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Pagination from "./components/Pagination";
 
-export default function Home() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={2}/>;
+interface Props {
+  searchParams: Promise<{ page?: string }>;
+}
+
+export default async function Home({ searchParams }: Props) {
+  const params = await searchParams;
+
+  const page = parseInt(params.page || "1");
+  const currentPage = isNaN(page) || page < 1 ? 1 : page;
+
+  return <Pagination itemCount={100} pageSize={10} currentPage={currentPage} />;
 }
