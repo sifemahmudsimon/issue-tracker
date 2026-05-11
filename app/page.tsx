96fe7ac0a45231243fcd083b3cff763d832f3da9
simon <simon@mirinfosys.com>
2026-05-11 13:09:49 +0600

    build issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+import { prisma } from "@/lib/prisma";
 import Pagination from "./components/Pagination";
+import IssueSumarry from "./IssueSumarry";
 import LatestIssues from "./LatestIssues";
 
 export default async function Home() {
-  return <LatestIssues />;
+  const open = await prisma.issue.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
+
+  console.log({ open, inProgress, closed });
+  return (
+    <>
+      <LatestIssues />
+      <IssueSumarry open={open} inProgress={inProgress} closed={closed} />
+    </>
+  );
 }
